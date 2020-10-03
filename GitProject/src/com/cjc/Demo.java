49204fc4a094abb2538cd20c00bb8bcbebeac330
amit <amit@DESKTOP-SIU2A5U.mshome.net>
2020-10-03 12:07:25 +0530

    by Amit

--- GitProject/src/com/cjc/Demo.java
+++ GitProject/src/com/cjc/Demo.java
@@ -2,6 +2,11 @@ package com.cjc;
 
 public class Demo 
 {
+	
+	public void sum()
+	{
+		System.out.println("Sum");
+	}
 public static void main(String[] args) {
 	
 	System.out.println("Hello Main Method");
